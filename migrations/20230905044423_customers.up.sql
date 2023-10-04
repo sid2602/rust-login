ae78959715a4411a445dcaffe9f5fe41e6a49fb3
sid2602 <kornaus.filip@gmail.com>
2023-10-04 18:25:46 +0200

    add created_at, updated_at fields and also auto generate id for user

--- migrations/20230905044423_customers.up.sql
+++ migrations/20230905044423_customers.up.sql
@@ -1,13 +1,15 @@
--- Add up migration script here
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TYPE customer_role AS ENUM ('admin', 'staff', 'viewer');
 
 CREATE TABLE customers (
-    id uuid PRIMARY KEY ,
+    id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
     firstname varchar(255) NOT NULL,
     lastname varchar(255) NOT NULL,
     password varchar(255) NOT NULL,
     email varchar(255) NOT NULL,
     "role" customer_role NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
 
     UNIQUE (email)
 );
